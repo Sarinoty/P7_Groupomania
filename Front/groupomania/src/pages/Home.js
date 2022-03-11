d7ddcc4383d31b5962d52a70dd5e39c1894b3df7
Sarinoty <contact@sarinoty.com>
2022-03-11 10:42:46 +0100

    Animation d'entrée, first step.

--- Front/groupomania/src/pages/Home.js
+++ Front/groupomania/src/pages/Home.js
@@ -6,6 +6,11 @@ import '../styles/SignInUp.scss';
 const Home = () => {
     return (
         <div className='container'>
+            <div className='loader'>
+                <div className='imgContainer'>
+                    <img src='./imgs/icon-left-font2.png' alt='Logo de la société Groupomania' className='imgContainer__img' />
+                </div>
+            </div>
             <Logo />
             <div className='sous-container' >
                 <Log />
